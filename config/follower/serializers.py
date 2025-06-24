c432805075beb6b8704d7fd7bf18f61874a13d17
Ali Rostami <AliRostami1253@gmail.com>
2025-06-24 09:15:46 +0330

    update serializers.py

--- config/follower/serializers.py
+++ config/follower/serializers.py
@@ -4,6 +4,9 @@ from .models import Follower
 
 
 class FollowerSerializers(serializers.ModelSerializer):
+    follower = serializers.CharField(read_only=True, source="follower.username")
+    followed = serializers.CharField(read_only=True, source="followed.username")
+
     class Meta:
         model = Follower
         fields = ['id', 'follower', 'followed']
